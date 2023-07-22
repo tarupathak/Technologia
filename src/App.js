e63f4ea64fee34ca8e4eecbdab2eb00765bcb1db
tarupathak <113367480+tarupathak@users.noreply.github.com>
2023-07-22 17:40:17 +0530

    Made navbar

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import React from "react";
+import Navbar from "./Components/Navbar";
+import bg from './Assets/Bg-img.webp'
+import './App.css'
 
 function App() {
-  return <div></div>;
+  return <div>
+    <Navbar />
+    <img src={bg} alt="bg-img" className="bg-img"/>
+  </div>;
 }
 
 export default App;
---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import "./Components.css";
+import logo from "../Assets/Logo-white.svg";
+import dropdown from '../Assets/dropdown.svg'
+
+const Navbar = () => {
+  return (
+    <div id="navbar">
+      <div id="logo">
+        <img src={logo} alt="logo-img" loading="lazy" />
+      </div>
+      <div id="links">
+        <div>Home <img src={dropdown} alt="dropdown" className="drop"/></div>
+        <div>Solution <img src={dropdown} alt="dropdown" className="drop"/></div>
+        <div>Company <img src={dropdown} alt="dropdown" className="drop"/></div>
+        <div>Resources <img src={dropdown} alt="dropdown" className="drop"/></div>
+        <div>Portfolio</div>
+      </div>
+      <div id="touch-btn">
+        <button>Get in touch</button>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
